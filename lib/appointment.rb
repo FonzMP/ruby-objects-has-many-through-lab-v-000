513824c17f4454a371f52960cea747526c2f33e2
Alex Wilson <fonzmp@gmail.com>
2018-05-21 04:58:01 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,5 +3,10 @@ class Appointment
   attr_accessor :patient, :doctor, :date
 
   def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
+  end
 
 end
